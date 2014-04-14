c450169ab7ec96eeb00f2dcdbc41044efc7bd60b
Zach Kanzler <they4kman@gmail.com>
2014-04-14 18:02:46 -0400

    Adding blink in between symptoms

--- neuro-collector/neuro_collector/clients/symptoms/__init__.py
+++ neuro-collector/neuro_collector/clients/symptoms/__init__.py
@@ -9,9 +9,17 @@ pygame.init()
 from neuro_collector.client import NeuroClient
 from neuro_collector.clients.symptoms.wrap import render_wrap
 
-
+# CONFIGURE TIME HERE
 TIME_SYMPTOM_ON_SCREEN = 3000
-TIME_SYMPTOM_OFF_SCREEN = 1000
+TIME_BLINK_ON_SCREEN = 500
+TIME_SCREEN_BLANK = 1000
+# DON'T CHANGE ANY CODE BELOW
+
+
+# Enum used to identify unique screens
+DISPLAY_BLANK = 0
+DISPLAY_SYMPTOM = 1
+DISPLAY_BLINK = 2
 
 
 curdir = os.path.dirname(__file__)
@@ -38,45 +46,69 @@ screen = pygame.display.set_mode((640, 480), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
 halt = False
-symptom_on_screen = False
+display_cur = DISPLAY_BLANK
+display_next = DISPLAY_SYMPTOM
 
-font = pygame.font.SysFont('Arial', 48)
+symptom_font = pygame.font.SysFont('Arial', 48)
 
 explanation_font = pygame.font.SysFont('Arial', 30)
 explanation = explanation_font.render('Do you experience...', True, (0, 0, 0))
 
+blink_font = pygame.font.SysFont('Arial', 72)
+blink_text = blink_font.render('BLINK', True, (0, 0, 0))
 
-pygame.time.set_timer(pygame.USEREVENT, TIME_SYMPTOM_OFF_SCREEN)
 
-while not halt:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or not have_more_symptoms():
-            halt = True
-            break
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            halt = True
-            break
-        elif event.type == pygame.USEREVENT:
-            if symptom_on_screen:
-                symptom_on_screen = False
-                pygame.time.set_timer(pygame.USEREVENT, TIME_SYMPTOM_OFF_SCREEN)
-                client.record_sensors({'symptom': '', 'group': ''})
-            else:
-                symptom_on_screen = True
-                group, symptom = get_next_symptom()
-                client.record_sensors({'symptom': symptom, 'group': group})
+pygame.time.set_timer(pygame.USEREVENT, TIME_SCREEN_BLANK)
 
-                pygame.time.set_timer(pygame.USEREVENT, TIME_SYMPTOM_ON_SCREEN)
-                text = render_wrap(symptom, font, 620)
+try:
+    while not halt:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or not have_more_symptoms():
+                halt = True
+                break
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                halt = True
+                break
+            elif event.type == pygame.USEREVENT:
+                if display_cur == DISPLAY_BLANK:
+                    display_cur = display_next
 
-    screen.fill((255, 255, 255))
-    screen.blit(explanation, (320 - explanation.get_width() // 2, 0))
+                    if display_cur == DISPLAY_SYMPTOM:
+                        group, symptom = get_next_symptom()
+                        client.record_sensors({'symptom': symptom, 'group': group})
+                        symptom_text = render_wrap(symptom, symptom_font, 620)
 
-    if symptom_on_screen:
-        screen.blit(text, (320 - text.get_width() // 2,
-                           240 - text.get_height() // 2))
+                        display_next = DISPLAY_BLINK
+                        pygame.time.set_timer(pygame.USEREVENT,
+                                              TIME_SYMPTOM_ON_SCREEN)
+                    else:
+                        client.record_sensors({'blink': 'blink'})
 
-    pygame.display.flip()
-    clock.tick(60)
-else:
-    sys.exit(0)
+                        display_next = DISPLAY_SYMPTOM
+                        pygame.time.set_timer(pygame.USEREVENT,
+                                              TIME_BLINK_ON_SCREEN)
+
+                else:
+                    display_cur = DISPLAY_BLANK
+                    pygame.time.set_timer(pygame.USEREVENT, TIME_SCREEN_BLANK)
+                    client.record_sensors({'symptom': '', 'group': '', 'blink': ''})
+
+        screen.fill((255, 255, 255))
+
+        if display_cur == DISPLAY_SYMPTOM:
+            screen.blit(explanation, (320 - explanation.get_width() // 2, 0))
+            screen.blit(symptom_text, (320 - symptom_text.get_width() // 2,
+                                       240 - symptom_text.get_height() // 2))
+        elif display_cur == DISPLAY_BLINK:
+            screen.blit(blink_text, (320 - blink_text.get_width() // 2,
+                                     240 - blink_text.get_height() // 2))
+
+        pygame.display.flip()
+        clock.tick(60)
+    else:
+        sys.exit(0)
+finally:
+    try:
+        pygame.display.set_mode((640, 480))
+    except Exception:
+        pass
